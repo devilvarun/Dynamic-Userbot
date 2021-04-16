f71110d5e6b704045aec1b9221bc77c13b4eba1b
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-16 21:14:31 +0530

    DYNAMIC VERSION 0.0.1

--- DYNAMIC/pulgins/alive.py
+++ DYNAMIC/pulgins/alive.py
@@ -11,10 +11,13 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**DYNAMIC USERBOT**"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`BOT IS WORKING FINE!` **ψ(｀∇´)ψ**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
-                     "`BOT VERSION : 0.0.1\nPython: 3.9.8\n`"
-                    # MAke By AMAN PANDEY
-                     "`Bot created by:` [AMAN PANDEY](tg://user?id=1503668700), @AmanPandeyDeveloperIN\n"
-                     f"`My  owner`: {DEFAULTUSER}\n\n"
-                     "https://github.com/DynamicUserbot/Dynamic-Userbot/")
+    await alive.edit(" ◆◆️{BOT}◆◆️  IS ON DUTY SIR
+                       "••••••SYSTEM STATUS••••••\
+                       ●●Telethon∆Version●●: 1.19.5\n
+                       ●●DYNAMIC∆Version●●: 0.0.1\n"
+                        f"●●【Uptime】●●: {uptm}\n""
+                        "●●My∆Master●●: {DEFAULTUSER} (tg://user?id=%7Btag%7D)\n"
+                        "●My∆Group●●: SUPPORT (https://t.me/DARKLON_USERBOT_SUPPORT)\n"
+                        "●●My∆Channel●●: CHANNEL (https://t.me/DARKLONXOP)\n"
+                        "●●OffTopic∆Group●●: OT (https://t.me/DARKLON_OT)n"
+                         "Want to Deploy me??? : [𝙳𝙴𝙿𝙻𝙾𝚈 (https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FHACKERBOTTELEGRAM%2FDARKLON-PACK&template=https%3A%2F%2Fgithub.com%2FHACKERBOTTELEGRAM%2FDARKLON-PACK) 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 𝙾𝙿 {BOT} (https://github.com/HACKERBOTTELEGRAM/HACKERBOTOP)  💠\n"   ")
