dff3f44e3238ffc9ad1da1a965df31862be9b71d
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-16 15:50:04 +0530

    Update __init__.py

--- DYNAMIC/__init__.py
+++ DYNAMIC/__init__.py
@@ -7,3 +7,17 @@ from telethon.session import StringSession
 from telethon import TelegramClient
 from var import var
 dynamic version = "0.0.1"
+
+os.system("pip install --upgrade pip")
+if Var.STRING_SESSION:
+    session_name = str(Var.STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+
+CMD_LIST = {}
+# for later purposes
+CMD_HELP = {}
+INT_PLUG = ""
+LOAD_PLUG = {}
